8b00c4b387f406b3a3eddbc23d9ab03ad60c7741
Ivo Jimenez <ivo.jimenez@gmail.com>
2016-01-09 00:05:43 -0800

    Update profiling_samples.sh

--- examples/profiling_samples.sh
+++ examples/profiling_samples.sh
@@ -1,5 +1,5 @@
 # ping
-./profile -f "--net=host" -i debian:jessie -c "ping google.com -c 2 " -n ping
+./profile -f "--net=host" -i debian:jessie -c "-c 2 ping google.com" -n ping
 
 # assuming gcc, g++ and make are available in the container
 ./profile -r -i repo:image -c "ls" -n ls-with-mica-recompiled
